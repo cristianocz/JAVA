c67e341fdd8b3e7d79252a572f0c4237fb346186
Cristiano Cezar <cristianocz@hotmail.com>
2024-06-17 21:07:32 -0300

    Iniando os treinamentos em Java

---
+++ Primeiro_Projeto_em_Java/java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!"); 
+        //imprime uma mensagem na saída padrão
+        final String BR = "Brasil" ;
+
+    }    
+}
